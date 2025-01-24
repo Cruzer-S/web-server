d6cdf87a0dd15a682f0c9eb911136fa24b229a2a
Cruzer-S <yyyynoom@gmail.com>
2025-01-25 03:44:03 +0900

    fix trivial bugs

--- config/run.mk
+++ config/run.mk
@@ -1,1 +1,1 @@
-null 443 
+null 443 resources/ certs/fullchain.pem certs/privkey.pem
--- source/main.c
+++ source/main.c
@@ -44,7 +44,7 @@ static void request_handler(Session session)
 	{
 	case HTTP_REQUEST_GET:
 		if ( !strcmp(header->url, "/") )
-			file = "resources/index.html";
+			file = "index.html";
 		else if ( !strcmp(header->url, "/favicon.ico") )
 			file = "favicon.png";
 		else
